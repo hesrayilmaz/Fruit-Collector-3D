f266cedf74627c344e60d4b8a57a0fc46f1bc250
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-04-24 15:16:00 +0300

    update player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,20 +7,49 @@ public class PlayerController : MonoBehaviour
     public float forwardSpeed;
     public float horizontalSpeed;
 
+    public float jumpForce;
+    private Rigidbody rb;
+    //public bool isPlayerOnGround = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // direction.z = forwardSpeed;
+
         transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed, Space.World);
 
         if (Input.GetKey(KeyCode.LeftArrow) && this.gameObject.transform.position.x>GroundBoundary.leftSide)
         {
             transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+           
         }
         else if (Input.GetKey(KeyCode.RightArrow) && this.gameObject.transform.position.x < GroundBoundary.rightSide)
         {
             transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed);
+            
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //isPlayerOnGround = false;
+        }
+
+    }
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            isPlayerOnGround = true;
         }
     }
-
+    */
 }
